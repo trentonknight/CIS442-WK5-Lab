16d0bf7a7b442bd5bd9638c6433280c7a25d4262
trentonknight <trentonknight@gmail.com>
2012-11-24 23:32:18 -0500

    close...

--- loadtabs.sql
+++ loadtabs.sql
@@ -7,150 +7,150 @@ DELETE FROM student;
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (1, 'Samuel', 'Smith', 'D', 'CS', '1234 Main Street', 
+  VALUES (newID.NEXTVAL, 'Samuel', 'Smith', 'D', 'CS', '1234 Main Street', 
            'Monument', 'CO', '80902', '719 260-1234', 100);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (2, 'Marylin', 'Manson', 'M', 'CIS', '246 Tejon Street', 
+  VALUES (newID.NEXTVAL, 'Marylin', 'Manson', 'M', 'CIS', '246 Tejon Street', 
            'Colorado Springs', 'CO', '80909', '719 570-2468', 102);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (3, 'Joanne', 'Wilson', 'D', 'HIS', '579 N. Nevada', 
+  VALUES (newID.NEXTVAL, 'Joanne', 'Wilson', 'D', 'HIS', '579 N. Nevada', 
            'Colorado Springs', 'CO', '80919', '719 575-1001', 107);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (4, 'Manny', 'Starr', ' ', 'BUS', '529 Flying W Ranch Road',
+  VALUES (newID.NEXTVAL, 'Manny', 'Starr', ' ', 'BUS', '529 Flying W Ranch Road',
            'Colorado Springs', 'CO', '80920', '719 330-1072', 102);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (5, 'Pat', 'Pullman', 'R', 'ECO', '468 Centennial Blvd, Apt. E',
+  VALUES (newID.NEXTVAL, 'Pat', 'Pullman', 'R', 'ECO', '468 Centennial Blvd, Apt. E',
            'Colorado Springs', 'CO', '80919', '719 358-5422', 103);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (6, 'Tim', 'Brown', 'F', 'ENG', '12 Bijou Street',
+  VALUES (newID.NEXTVAL, 'Tim', 'Brown', 'F', 'ENG', '12 Bijou Street',
            'Colorado Springs', 'CO', '80907', '719 651-1234' , 107);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (7, 'Barbara', 'Runza', 'W', 'ECO', '722 Garden of the Gods Road',
+  VALUES (newID.NEXTVAL, 'Barbara', 'Runza', 'W', 'ECO', '722 Garden of the Gods Road',
            'Colorado Springs', 'CO', '80920', '719 531-4472', 100 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (8, 'Dave', 'Elgass', 'R', 'MUS', '222 North Nevada',
+  VALUES (newID.NEXTVAL, 'Dave', 'Elgass', 'R', 'MUS', '222 North Nevada',
            'Colorado Springs', 'CO', '80920', '719 570-6543', 109);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (9, 'Maria', 'Roberts', 'M', 'NUR', '540-A Fillmore',
+  VALUES (newID.NEXTVAL, 'Maria', 'Roberts', 'M', 'NUR', '540-A Fillmore',
            'Colorado Springs', 'CO', '80920', '719 395-5431', 107);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (10, 'Dean', 'Calhoun', ' ', 'MAT', '6712 Blue Bird Drive',
+  VALUES (newID.NEXTVAL, 'Dean', 'Calhoun', ' ', 'MAT', '6712 Blue Bird Drive',
            'Colorado Springs', 'CO', '80903', '719 264-7074', 102 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (11, 'Barbara', 'Runza', 'W', 'ECO', '722 Garden of the Gods Road',
+  VALUES (newID.NEXTVAL, 'Barbara', 'Runza', 'W', 'ECO', '722 Garden of the Gods Road',
            'Colorado Springs', 'CO', '80920', '719 531-4472', 110 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (12, 'Steve', 'Knight', 'R', 'BUS', '456 East Pikes Peak',
+  VALUES (newID.NEXTVAL, 'Steve', 'Knight', 'R', 'BUS', '456 East Pikes Peak',
            'Manitou Springs', 'CO', '80190', '719 345-9007', 100 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (13, 'Martha', 'Lowell', 'Q', 'UND', '3458 Fountain Blvd.',
+  VALUES (newID.NEXTVAL, 'Martha', 'Lowell', 'Q', 'UND', '3458 Fountain Blvd.',
            'Aurora', 'CO', '80220', '303 576-6891', 105);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (14, 'Debra', 'Calhoun', ' ', 'MAT', '6712 Broken Wing Street',
+  VALUES (newID.NEXTVAL, 'Debra', 'Calhoun', ' ', 'MAT', '6712 Broken Wing Street',
            'Denver', 'CO', '80235', '303 388-1290', 104);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (15, 'Andrew', 'Baxter', 'D', 'CS', '1234 Main Street', 
+  VALUES (newID.NEXTVAL, 'Andrew', 'Baxter', 'D', 'CS', '1234 Main Street', 
            'Monument', 'CO', '80902', '719 234-1234', 109 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (16, 'Mary', 'Mitchell', 'M', 'CIS', '246 Tejon Street', 
+  VALUES (newID.NEXTVAL, 'Mary', 'Mitchell', 'M', 'CIS', '246 Tejon Street', 
            'Denver', 'CO', '80229', '303 450-2668', 106);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (17, 'Joe', 'Shahan', 'D', 'PS', '1625Woodmor Dr.', 
+  VALUES (newID.NEXTVAL, 'Joe', 'Shahan', 'D', 'PS', '1625Woodmor Dr.', 
            'Monument', 'CO', '80931', '719 354-1621', 100);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (18, 'Jeff', 'Schott', 'R', 'ENG', '1234 Main Dr.', 
+  VALUES (newID.NEXTVAL, 'Jeff', 'Schott', 'R', 'ENG', '1234 Main Dr.', 
            'Englewood', 'CO', '80931', '303 354-1621', 109);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (19, 'Jim', 'Fry', ' ', 'BUS', '529 E. Boulder Street',
+  VALUES (newID.NEXTVAL, 'Jim', 'Fry', ' ', 'BUS', '529 E. Boulder Street',
            'Colorado Springs', 'CO', '80920', '719 330-4245', 107 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (20, 'Patrick', 'Zbylski', 'R', 'ECO', '468 Bedford Lane',
+  VALUES (newID.NEXTVAL, 'Patrick', 'Zbylski', 'R', 'ECO', '468 Bedford Lane',
            'Colorado Springs', 'CO', '80919', '719 358-5334', 102 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (21, 'Thom', 'Black', 'F', 'ENG', '123 E Ramona',
+  VALUES (newID.NEXTVAL, 'Thom', 'Black', 'F', 'ENG', '123 E Ramona',
            'Colorado Springs', 'CO', '80907', '719 651-2345', 101 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (22, 'Tanya', 'Dunham', 'W', 'ECO', '2301 Ausint Bluffs Pkwy',
+  VALUES (newID.NEXTVAL, 'Tanya', 'Dunham', 'W', 'ECO', '2301 Ausint Bluffs Pkwy',
            'Monument', 'CO', '80902', '719 531-4427', 108 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (23, 'Mitch', 'Jaeger', 'R', 'MUS', '222 Vermijo',
+  VALUES (newID.NEXTVAL, 'Mitch', 'Jaeger', 'R', 'MUS', '222 Vermijo',
            'Colorado Springs', 'CO', '80920', '719 565-4356', 110 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (24, 'Lisa', 'Lawson', 'M', 'NUR', '4560 Fillmore',
+  VALUES (newID.NEXTVAL, 'Lisa', 'Lawson', 'M', 'NUR', '4560 Fillmore',
            'Colorado Springs', 'CO', '80920', '719 394-1231', 100 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (25, 'Jill', 'Perry', ' ', 'MAT', '6712 Hancock Expressway',
+  VALUES (newID.NEXTVAL, 'Jill', 'Perry', ' ', 'MAT', '6712 Hancock Expressway',
            'Colorado Springs', 'CO', '80903', '719 254-7874', 103 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (26, 'Bob', 'Tibbs', 'W', 'ECO', '7322 Carefree North',
+  VALUES (newID.NEXTVAL, 'Bob', 'Tibbs', 'W', 'ECO', '7322 Carefree North',
            'Colorado Springs', 'CO', '80920', '719 531-7244', 104 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (27, 'Sam', 'Crepps', 'R', 'BUS', '456 East Palmer Park',
+  VALUES (newID.NEXTVAL, 'Sam', 'Crepps', 'R', 'BUS', '456 East Palmer Park',
            'Manitou Springs', 'CO', '80190', '719 345-3297', 102);
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (28, 'Stephen', 'Austin', 'F', 'UND', '3458 S. Union Blvd.',
+  VALUES (newID.NEXTVAL, 'Stephen', 'Austin', 'F', 'UND', '3458 S. Union Blvd.',
            'Colorado Springs', 'CO', '80920', '719 357-4491', 105 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (29, 'Tom', 'Peyton', ' ', 'MAT', '6712 Jackson Street',
+  VALUES (newID.NEXTVAL, 'Tom', 'Peyton', ' ', 'MAT', '6712 Jackson Street',
            'Monument', 'CO', '80902', '719 591-1953', 108 );
 
 INSERT INTO student 
     (stdid, fname, lname, mi, majorid, address, city, state, zip, phone, advid )
-  VALUES (30, 'Tim', 'Presley', ' ', 'CIS', '8812 Federal Boulevard',
+  VALUES (newID.NEXTVAL, 'Tim', 'Presley', ' ', 'CIS', '8812 Federal Boulevard',
            'Denver', 'CO', '80222', '303 458-1953', 110 );
